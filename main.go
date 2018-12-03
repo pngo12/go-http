ec9fd496aca9649f8aa68d148c7568148370cdb1
pngo12 <pngo12@gmail.com>
2018-12-02 19:26:46 -0800

    delete & put

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
-import(
+import (
 	"encoding/json"
 	"log"
-	"net/http"
 	"math/rand"
+	"net/http"
 	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
 // Book Struct (Model)
 type Book struct {
-	ID 			string `json:"id"`
-	Isbn 		string `json:"isbn"`
-	Title 		string `json:"title"`
-	Author 		*Author `json:"author"`
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
 }
 
 // Author Struct
 type Author struct {
-	Firstname 	string `json:"firstname"`
-	Lastname 	string `json:"lastname"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
 }
 
 var books []Book
-
 
 // Get all books
 func getAllBooks(w http.ResponseWriter, r *http.Request) {
@@ -38,9 +38,9 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 
 	// Loop through the books and find the id
 	for _, item := range books {
-		if item.ID == params["id"]{
+		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
-			return 
+			return
 		}
 	}
 	json.NewEncoder(w).Encode(&Book{})
@@ -56,26 +56,39 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = strconv.Itoa(rand.Intn(10000000)) // Chance to generate the same ID
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
-
-
-
-
-
 
 func main() {
 	router := mux.NewRouter()
 
-	
-	books = append(books, Book{ID: "1", Isbn: "4455", Title: "Book One", Author: 
-	&Author{Firstname: "Joe", Lastname: "Dirt"}})
-	books = append(books, Book{ID: "2", Isbn: "5566", Title: "Book Two", Author: 
-	&Author{Firstname: "Molly", Lastname: "Sands"}})
+	books = append(books, Book{ID: "1", Isbn: "4455", Title: "Book One", Author: &Author{Firstname: "Joe", Lastname: "Dirt"}})
+	books = append(books, Book{ID: "2", Isbn: "5566", Title: "Book Two", Author: &Author{Firstname: "Molly", Lastname: "Sands"}})
 
 	router.HandleFunc("/api/books", getAllBooks).Methods("GET")
 	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
