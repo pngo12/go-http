09faff4f4a96e97275f255d3b733e1c5f4db77d2
pngo12 <pngo12@gmail.com>
2018-12-02 19:28:27 -0800

    final edit to put

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 			books = append(books[:index], books[index+1:]...)
 			var book Book
 			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = strconv.Itoa(rand.Intn(10000000)) // Chance to generate the same ID
+			book.ID = params["id"]
 			books = append(books, book)
 			json.NewEncoder(w).Encode(book)
 			return
