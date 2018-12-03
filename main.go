0f1326bb45974037d71c33010070da49b0d7a5d7
pngo12 <pngo12@gmail.com>
2018-12-02 19:19:09 -0800

    go REST api - get/create

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import(
+	"encoding/json"
+	"log"
+	"net/http"
+	"math/rand"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+// Book Struct (Model)
+type Book struct {
+	ID 			string `json:"id"`
+	Isbn 		string `json:"isbn"`
+	Title 		string `json:"title"`
+	Author 		*Author `json:"author"`
+}
+
+// Author Struct
+type Author struct {
+	Firstname 	string `json:"firstname"`
+	Lastname 	string `json:"lastname"`
+}
+
+var books []Book
+
+
+// Get all books
+func getAllBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	// Loop through the books and find the id
+	for _, item := range books {
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return 
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000)) // Chance to generate the same ID
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+
+
+
+
+
+func main() {
+	router := mux.NewRouter()
+
+	
+	books = append(books, Book{ID: "1", Isbn: "4455", Title: "Book One", Author: 
+	&Author{Firstname: "Joe", Lastname: "Dirt"}})
+	books = append(books, Book{ID: "2", Isbn: "5566", Title: "Book Two", Author: 
+	&Author{Firstname: "Molly", Lastname: "Sands"}})
+
+	router.HandleFunc("/api/books", getAllBooks).Methods("GET")
+	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/api/new", createBook).Methods("POST")
+	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/api/books/{id}", deleteBook).Methods("Delete")
+
+	log.Fatal(http.ListenAndServe(":5000", router))
+}
